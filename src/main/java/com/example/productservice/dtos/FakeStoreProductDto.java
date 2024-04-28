4fc7005db7a3d6191004ffcd642599e036934daa
Akshit Gupta <59478377+Akshit-1807@users.noreply.github.com>
2024-04-28 16:22:48 +0530

    git commit

--- src/main/java/com/example/productservice/dtos/FakeStoreProductDto.java
+++ src/main/java/com/example/productservice/dtos/FakeStoreProductDto.java
@@ -1,5 +1,10 @@
 package com.example.productservice.dtos;
 
 public class FakeStoreProductDto {
+    ///DTO -> Data Transfer Objects
+    private Long id;
+    private String title;
+    private int price;
+    private String category;
 
 }
