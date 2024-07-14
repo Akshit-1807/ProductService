f9faed47c98a51a5ecfefc54fa18a45dbf16fa01
Akshit-1807 <gakshit52@gmail.com>
2024-07-14 17:32:37 +0530

    git commit -m "ProductService"

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,14 +1,21 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product extends BaseModel{
+@Entity
+public class Product extends BaseModel {
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+
+    //category is not a primitive attribute. It is a relation.
+    @ManyToOne
     private Category category;
 }
+
